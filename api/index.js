42a654efd0b672b7dd5f7004fded90a64d6628b9
Mburu <abdallwario96@gmail.com>
2023-05-09 11:58:30 +0300

    vercel configs

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 
 // app.use(cors({credentials:true,origin:'https://blogsite-rosy.vercel.app'}));
 
-app.use(cors({credentials:true,origin:'http://localhost:5173'}));
+app.use(cors());
 
 
 app.use(express.json())
